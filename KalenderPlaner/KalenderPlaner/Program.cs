3d9f893d6b7cf2f24690565141e0cffa4adc066b
Tina Plavius <Tina@Plavius.de>
2015-06-23 10:25:52 +0200

    Projekterstellung

---
+++ KalenderPlaner/KalenderPlaner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalenderPlaner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
