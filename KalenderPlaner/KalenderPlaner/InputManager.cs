053d799bdc88aea79c64a14fb99deae1c0054bfa
Max Tratnik <your@email.com>
2015-06-23 13:31:30 +0200

    Input-Manager eingebaut
    (+ Test-Dokument (File.txt) hinzugefügt)

---
+++ KalenderPlaner/KalenderPlaner/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalenderPlaner
+{
+    static class InputManager
+    {
+        public static string FilePath;
+        public static string Data;
+
+        public static bool Parse(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                try
+                {
+                    FilePath = Path.GetFullPath(args[0]);
+                    if (File.Exists(FilePath))
+                    {
+                        Data = File.ReadAllText(args[0]);
+                        return true;
+                    }
+                    else
+                    {
+                        throw new FileNotFoundException();
+                    }
+                }
+                catch (FileNotFoundException e)
+                {
+                    WriteError("Kein gültiges Dokument gefunden.");
+                } 
+                catch (Exception e)
+                {
+                    WriteError("Ausnahme aufgetreten: " + e.Message);
+                } 
+            }
+            return false;
+        }
+
+        private static void WriteError(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(Environment.NewLine + message + Environment.NewLine);
+            Console.ForegroundColor = Program.DefaultColor;
+        }
+    }
+}
--- KalenderPlaner/KalenderPlaner/Program.cs
+++ KalenderPlaner/KalenderPlaner/Program.cs
@@ -8,10 +8,28 @@ namespace KalenderPlaner
 {
     class Program
     {
+
+        public static ConsoleColor DefaultColor = ConsoleColor.Gray;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Project started!");
-            Console.ReadKey();
+            Console.Title = "Kalenderplaner";
+
+            if (InputManager.Parse(args))
+            {
+                Console.WriteLine("Memberdaten eingelesen.");
+                Console.WriteLine();
+                Console.WriteLine("Inhalt:");
+                Console.WriteLine(InputManager.Data);
+            }
+            else
+            {
+                Console.WriteLine("Fehler beim Einlesen der Member-Daten!");
+            }
+
+            
+            Console.ReadKey(true);
         }
     }
 }
