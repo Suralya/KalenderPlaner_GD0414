e8539bf9f79a3059d796b3b2e20c22f7fe1ff2c2
Max Tratnik <max.tratnik@gmail.com>
2015-06-26 12:03:33 +0200

    * JsonConverter - Convert String to Datetime/span (WIP!)

--- KalenderPlaner/KalenderPlaner/JsonConverter.cs
+++ KalenderPlaner/KalenderPlaner/JsonConverter.cs
@@ -1,16 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
 namespace KalenderPlaner
 {
+
+    [Serializable]
+    public class StringToDateConvertException : Exception
+    {
+        //
+        // For guidelines regarding the creation of new exception types, see
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/cpgenref/html/cpconerrorraisinghandlingguidelines.asp
+        // and
+        //    http://msdn.microsoft.com/library/default.asp?url=/library/en-us/dncscol/html/csharp07192001.asp
+        //
+
+        public StringToDateConvertException()
+        {
+        }
+
+        public StringToDateConvertException(string message)
+            : base(message)
+        {
+        }
+
+        public StringToDateConvertException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        protected StringToDateConvertException(
+            SerializationInfo info,
+            StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+
+
     class JsonConverter
     {
-        //convertiert string in Jason und andersrum
+        public List<Timespan> OnceSpans = new List<Timespan>();
+        public List<Timespan> PermSpans = new List<Timespan>();
 
+
+        //convertiert string in Jason und andersrum
         public JsonConverter()
         {
         }
@@ -43,5 +82,51 @@ namespace KalenderPlaner
             return lists.MemberList;
         }
 
+
+        public void SaveTimeCondition(string conditions)
+        {
+            string[] tempCond = conditions.Split(',');
+            if (tempCond.Length <= 1)
+                throw new StringToDateConvertException();
+            switch (tempCond[0].ToLower())
+            {
+                case "once":
+                    SaveOnceCondition(tempCond);
+                    break;
+                case "perm":
+                    SavePermCondition(tempCond);
+                    break;
+                default:
+                    throw new StringToDateConvertException();
+                    break;
+            }
+        }
+
+        private void SaveOnceCondition(string[] condition)
+        {
+            int min1, min2, hour1, hour2, day1, day2, month1, month2, year1, year2;
+            for (int i = 1; i < condition.Length; i++)
+            {
+                switch (condition.Length)
+                {
+                    case 5:
+
+                    case 4:
+
+                    case 3:
+
+                    case 2:
+
+                    default:
+                        throw new StringToDateConvertException();
+                        break;
+                }
+            }
+        }
+
+        private void SavePermCondition(string[] condition)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
