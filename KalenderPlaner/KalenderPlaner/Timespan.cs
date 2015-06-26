fccd7044c87af8043426a1790389fc81d1c493a0
Tina Plavius <Tina@Plavius.de>
2015-06-26 11:42:24 +0200

    Timespan class added

---
+++ KalenderPlaner/KalenderPlaner/Timespan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalenderPlaner
+{
+    class Timespan
+    {
+        public DateTime StarTime;
+        public DateTime EndTime;
+
+        public Timespan(DateTime starTime, DateTime endTime)
+        {
+            StarTime = starTime;
+            EndTime = endTime;
+        }
+
+        public Timespan(DateTime starTime) : this(starTime, starTime) { }
+    }
+}
