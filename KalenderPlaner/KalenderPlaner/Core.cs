b237a3881b77e985d2fc425c435481063c3b54f5
Ziturion <dmajonica@gmx.de>
2015-07-14 14:40:33 +0200

    *new TODO added
    Sachen überdacht. Keine Member werden random erzeugt sondern die Member werden random auf die Zeitspanne gesetzt

--- KalenderPlaner/KalenderPlaner/Core.cs
+++ KalenderPlaner/KalenderPlaner/Core.cs
@@ -10,7 +10,7 @@ namespace KalenderPlaner
 {
     class Core
     {
-        public List<Member> Selection = new List<Member>(); 
+        public List<Member> Selection;
 
         // Jeder Member sucht sich einen wieteren Member etc-( solange bis alle demands an Member gestillt sind ?)- dann sucht sich der Member Zeiten
 
@@ -19,28 +19,18 @@ namespace KalenderPlaner
 
         //Rückgabe einer Liste 
 
-        private Genome<List<Member>> GetMemberGenomes(List<Member> members)
-        {
-            return new Genome<List<Member>>(members);
-        }
 
         public List<Genome<List<Member>>> GenerateSolutions(int populationSize, List<Member> members)
         {
             var temp = new List<Genome<List<Member>>>();
             for (int i = 0; i < populationSize; i++)
             {
-                temp.Add(GetMemberGenomes(members));
+                temp.Add(new Genome<List<Member>>(members));
             }
             return temp;
         }
 
-        public void GenerateRandomMember(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                //Member mit zufallswerten füllen
-                Selection.Add(new Member());
-            }
-        }
+        //TODO Mehrere Selections auf die Zeitspanne verteilen
+
     }
 }
