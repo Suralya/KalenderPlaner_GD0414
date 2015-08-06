1bcb70d6c625034dfae04d84d65cbacc3fc5d183
Ziturion <dmajonica@gmx.de>
2015-08-06 15:22:09 +0200

    RandomGenom Erstellung der 1. Generation erstellt.

--- KalenderPlaner/KalenderPlaner/Core.cs
+++ KalenderPlaner/KalenderPlaner/Core.cs
@@ -1,21 +1,25 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using GeneticAlgorithm;
 
 namespace KalenderPlaner
 {
     class Core
     {
+        
         private double _crossoverProbability = 0.8;
         private double _mutationProbability = 0.1;
         private int _populationSize = 100;
         private int _generationCount = 500;
+        private List<Member> _members; 
 
-        public Core(double crossoverProbability, double mutationProbability, int populationSize, int generationCount)
+        public Core(double crossoverProbability, double mutationProbability, int populationSize, int generationCount, JsonConverter jsonConverter, RawInput rawInput)
         {
             _crossoverProbability = crossoverProbability;
             _mutationProbability = mutationProbability;
             _populationSize = populationSize;
             _generationCount = generationCount;
+            _members = jsonConverter.MembersGet(rawInput);
         }
 
         public List<Member> Selection;
@@ -42,14 +46,24 @@ namespace KalenderPlaner
         public List<Genome<List<Member>>> GenerateSolutions(List<Member> members)
         {
             var firstGeneration = new List<Genome<List<Member>>>();
+            var randomGen = RandomMembersAtTime(_members);
             for (int i = 0; i < _populationSize; i++)
             {
-                firstGeneration.Add(new Genome<List<Member>>()); //TODO zufälliges Genom
+                firstGeneration.Add(new Genome<List<Member>>(randomGen));
             }
             return firstGeneration;
         }
 
-        //TODO Mehrere Selections auf die Zeitspanne verteilen
+        private List<Member> RandomMembersAtTime(List<Member> members)
+        {
+            foreach (Member member in members)
+            {
+                List<Member> tmplist = members.Where(i => i.Demand.Any(j => j.Name == member.Name)).ToList();
+                List<Resource> tmplist2 = tmplist.Select(k => new Resource(k.Name)).ToList();
+                member.Offer.AddRange(tmplist2);
+            }
+            return members;
+        } 
 
     }
 }
