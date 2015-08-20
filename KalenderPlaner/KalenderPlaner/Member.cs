19bc04d23cf5d631eb26bbdd57ae1084f03cf685
Max Tratnik <max.tratnik@gmail.com>
2015-08-20 16:34:28 +0200

    * Ausgabe von Member-Liste in Console

--- KalenderPlaner/KalenderPlaner/Member.cs
+++ KalenderPlaner/KalenderPlaner/Member.cs
@@ -21,7 +21,7 @@ namespace KalenderPlaner
             BlockedDays = blockedDays;
             Itterations = itterations;
 
-            Datas = new List<DateTime> { new DateTime(0, 1, 1, new GregorianCalendar()) }; //TODO DateTime-Factory-Methode hier einsätzen
+            Datas = new List<DateTime> { new DateTime(1, 1, 1, new GregorianCalendar()) }; //TODO DateTime-Factory-Methode hier einsätzen
         }
 
         public Member(string name, List<Resource> offer, List<Resource> demand, List<DateTime> blockedDays) : this(name, offer, demand, blockedDays, -1) { }
